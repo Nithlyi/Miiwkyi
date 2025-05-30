d7b6256cf79f8de025885896ece9c14dc496871f
Tokisaki <hutaokurumiy@gmail.com>
2025-05-29 21:39:40 -0300

    Update reactionrole.js

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -1,103 +1,47 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
-const fs = require("fs");
-const path = require("path");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("reactionrole")
-    .setDescription("Configura uma reaction role numa mensagem existente")
+    .setDescription("Cria uma mensagem de reaction role.")
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles)
-    .addChannelOption((option) =>
-      option
-        .setName("canal")
-        .setDescription("Canal da mensagem")
-        .setRequired(true)
+    .addStringOption((opt) =>
+      opt.setName("mensagem").setDescription("Texto da mensagem").setRequired(true)
     )
-    .addStringOption((option) =>
-      option
-        .setName("mensagemid")
-        .setDescription("ID da mensagem onde será adicionada a reaction")
-        .setRequired(true)
+    .addRoleOption((opt) =>
+      opt.setName("cargo").setDescription("Cargo a ser atribuído").setRequired(true)
     )
-    .addRoleOption((option) =>
-      option
-        .setName("cargo")
-        .setDescription("Cargo que será dado/removido")
-        .setRequired(true)
-    )
-    .addStringOption((option) =>
-      option
-        .setName("emoji")
-        .setDescription("Emoji para a reaction (unicode ou emoji custom)")
-        .setRequired(true)
+    .addStringOption((opt) =>
+      opt.setName("emoji").setDescription("Emoji para o cargo").setRequired(true)
     ),
 
   async execute(interaction) {
-    const channel = interaction.options.getChannel("canal");
-    const messageId = interaction.options.getString("mensagemid");
+    const msg = interaction.options.getString("mensagem");
     const role = interaction.options.getRole("cargo");
     const emoji = interaction.options.getString("emoji");
-    const guild = interaction.guild;
 
-    if (!channel || channel.guild.id !== guild.id || channel.type !== 0) {
-      return interaction.reply({
-        content: "Canal inválido ou não é um canal de texto.",
-        ephemeral: true,
-      });
-    }
-
-    let message;
+    const sentMessage = await interaction.channel.send(msg);
     try {
-      message = await channel.messages.fetch(messageId);
+      await sentMessage.react(emoji);
     } catch (err) {
       return interaction.reply({
-        content: "Não encontrei a mensagem com esse ID nesse canal.",
+        content: "❌ Falha ao adicionar o emoji. Verifique se é válido.",
         ephemeral: true,
       });
     }
 
-    try {
-      await message.react(emoji);
-    } catch (err) {
-      return interaction.reply({
-        content:
-          "Não consegui reagir com esse emoji. Use um emoji válido (unicode ou custom).",
-        ephemeral: true,
-      });
-    }
-
-    const filePath = path.join(__dirname, "..", "reactionroles.json");
-    let reactionRoles = [];
-    try {
-      if (fs.existsSync(filePath)) {
-        const data = fs.readFileSync(filePath, "utf8");
-        reactionRoles = JSON.parse(data);
-      }
-    } catch {
-      reactionRoles = [];
-    }
-
-    // Adiciona nova reaction role
-    reactionRoles.push({
-      guildId: guild.id,
-      channelId: channel.id,
-      messageId: message.id,
-      emoji,
+    // Atualiza JSON
+    const newEntry = {
+      messageId: sentMessage.id,
+      emoji: emoji,
       roleId: role.id,
-    });
+    };
 
-    // Salva no arquivo
-    try {
-      fs.writeFileSync(filePath, JSON.stringify(reactionRoles, null, 2), "utf8");
-    } catch (err) {
-      return interaction.reply({
-        content: "Erro ao salvar as configurações de reaction role.",
-        ephemeral: true,
-      });
-    }
+    reactionRoles.push(newEntry);
+    saveReactionRoles();
 
     await interaction.reply({
-      content: `✅ Reaction role configurada! Usuários que reagirem com ${emoji} na mensagem <https://discord.com/channels/${guild.id}/${channel.id}/${message.id}> receberão o cargo **${role.name}**.`,
+      content: "✅ Reaction Role criado com sucesso.",
       ephemeral: true,
     });
   },
